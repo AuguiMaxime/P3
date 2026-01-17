3f72f483e4fb8575ceaa75651cb3010ee9eb9ebf
AuguiMaxime <auguimaxime.pro@gmail.com>
2026-01-17 16:00:46 +0100

    Initial commit

--- sophie/FrontEnd/script.js
+++ sophie/FrontEnd/script.js
@@ -1,125 +1,160 @@
+console.log("JS chargé");
 
 
-// récup des données Api //
+// ----------------------
+// PARTIE GALLERIE
+// ----------------------
+async function fetchWorks() { 
+    const response = await fetch("http://localhost:5678/api/works");
+     return await response.json(); }
+
+async function showGallery(works) {
+    // Si pas de galerie → on est sur login.html
+    const sectionGallery = document.querySelector(".gallery");
+    if (!sectionGallery) return;
+    sectionGallery.innerHTML = "";
+    
+// Affichage des works
+     works.forEach(article => {
+        const worksElement = document.createElement("article");
+        const imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl; 
+        const titleElement = document.createElement("h3"); 
+        titleElement.innerText = article.title;
+
+        worksElement.appendChild(imageElement); 
+        worksElement.appendChild(titleElement); 
+        sectionGallery.appendChild(worksElement); 
+    }); 
+}
+    // ----------------------
+    // BOUTONS FILTRES + init
+    // ----------------------
+
+   async function init() { 
+    const works = await fetchWorks(); 
+    showGallery(works); 
+    document.querySelector(".btn-objet").addEventListener("click", () => {
+         showGallery(works.filter(w => w.category.name === "Objets"));
+         });
+
+          document.querySelector(".btn-apt").addEventListener("click", () => {
+             showGallery(works.filter(w => w.category.name === "Appartements"));
+             }); 
+
+             document.querySelector(".btn-hotel").addEventListener("click", () => {
+                 showGallery(works.filter(w => w.category.name === "Hotels & restaurants")); }); 
+
+                 document.querySelector(".btn-tous").addEventListener("click", () => {
+                     showGallery(works);
+                     }); 
+                    } 
+                     init();
 
 
-const response = await fetch("http://localhost:5678/api/works");
-const works = await response.json();
+// ----------------------
+// PARTIE LOGIN
+// ----------------------
 
-// fonction for pour afficher toutes les images de l'api // 
+const form = document.getElementById("login-form");
 
-for (let i=0; i< works.length; i++) {
+if (form) {
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-const article = works[i]; 
-const sectionGallery = document.querySelector(".gallery")
-const worksElement = document.createElement("article");
+        const emailValue = document.getElementById("email").value;
+        const passwordValue = document.getElementById("password").value;
+
+        console.log("Email :", emailValue);
+        console.log("Password :", passwordValue);
+
+        login(emailValue, passwordValue);
+    });
+}
+
+async function login(emailValue, passwordValue) {
+
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: emailValue,
+                password: passwordValue
+            })
+        });
+        const data = await response.json();
+
+        if (data.token) {
+            console.log("Token reçu :", data.token);
+            sessionStorage.setItem("token", data.token);; //session au lieu de local storage pour éviter de garder le token//
+            window.location.href = "index.html";
+        } else {
+            alert("Erreur dans l’identifiant ou le mot de passe");
+        }
+
+    } catch (error) {
+        console.error("Erreur fetch :", error);
+        alert("Impossible de se connecter au serveur");
+    }
+}
 
 
-// création des articles de la section gallery //
+// MODE EDITION //
 
-const imageElement = document.createElement ("img");
-imageElement.src = article.imageUrl;
+/// Bouton Logout ///
+const logout = document.getElementById("logout")
+logout.addEventListener("click", function(event){
+    sessionStorage.removeItem("token");
+    window.location.reload();
+})
 
-const titleElement = document.createElement ("h3");
-titleElement.innerHTML = article.title;
+const token = sessionStorage.getItem("token");
 
-const nameElement = document.createElement ("p")
-nameElement.innerHTML = article.category.name;
+if (token) {
+    const editionBar = document.querySelector("#edition");
+    if (editionBar) {
+        editionBar.style.display = "flex";
+    }
 
+    const filtres = document.querySelector("#filtres");
+    if (filtres) {
+        filtres.style.display = "none";
+    }
 
-// rattachement des balises au dom //
+    const modify = document.querySelector("#modify");
+    if (modify) {
+        modify.style.display = "flex";
 
-sectionGallery.appendChild(worksElement)
-
-worksElement.appendChild(imageElement)
-worksElement.appendChild(titleElement)
+        const logged = document.querySelector("#login");
+    if (logged) {
+        logged.style.display = "none";
+    }
+        if (logout) {
+            logout.style.display = "flex"
+        }
+        const portfolio = document.querySelector("#portfolio")
+        if (portfolio) {
+            portfolio.style.display = "flex"
+        }
+    }
 }
 
 
 
-// Partie Bouton filtres //
-const boutonObjet = document.querySelector(".btn-objet");
-boutonObjet.addEventListener("click", function(){
+// Mode Edition // 
 
-    const worksObjets = works.filter(work => work.category.name === "Objets");
-
-    // On vide la galerie//
-    const sectionGallery = document.querySelector(".gallery"); sectionGallery.innerHTML = "";
-
-    worksObjets.forEach(article => { const imageElement = document.createElement("img");
-         imageElement.src = article.imageUrl;
-        const titleElement = document.createElement("h3");
-        titleElement.innerText = article.title;
-
-        const worksElement = document.createElement("article");
-       
-        worksElement.appendChild(imageElement);
-        worksElement.appendChild(titleElement);
-        sectionGallery.appendChild(worksElement)
-             });
-
-    });
-
-    const boutonAppartement = document.querySelector(".btn-apt");
-    boutonAppartement.addEventListener("click", function(){
-
-    const worksAppartements = works.filter(work => work.category.name === "Appartements");
-    const sectionGallery = document.querySelector(".gallery"); sectionGallery.innerHTML = "";
-
-    worksAppartements.forEach(article => { const imageElement = document.createElement("img");
-         imageElement.src = article.imageUrl;
-        const titleElement = document.createElement("h3");
-        titleElement.innerText = article.title;
-        
-        const worksElement = document.createElement("article");
-       
-        worksElement.appendChild(imageElement);
-        worksElement.appendChild(titleElement);
-        sectionGallery.appendChild(worksElement)
-             });
-
-    });
-   
-    const boutonHotels = document.querySelector(".btn-hotel");
-    boutonHotels.addEventListener("click", function(){
-
-    const worksHotel = works.filter(work => work.category.name === "Hotels & restaurants");
-    const sectionGallery = document.querySelector(".gallery"); sectionGallery.innerHTML = "";
-
-    worksHotel.forEach(article => { const imageElement = document.createElement("img");
-         imageElement.src = article.imageUrl;
-        const titleElement = document.createElement("h3");
-        titleElement.innerText = article.title;
-        
-        const worksElement = document.createElement("article");
-       
-        worksElement.appendChild(imageElement);
-        worksElement.appendChild(titleElement);
-        sectionGallery.appendChild(worksElement)
-             });
-
-    });
-
-const boutonTous = document.querySelector(".btn-tous");
-    boutonTous.addEventListener("click", function(){
-    const sectionGallery = document.querySelector(".gallery"); sectionGallery.innerHTML = ""; // on vide//
-
-    works.forEach(article => { const imageElement = document.createElement("img");
-         imageElement.src = article.imageUrl;
-        const titleElement = document.createElement("h3");
-        titleElement.innerText = article.title;
-        
-        const worksElement = document.createElement("article");
-       
-        worksElement.appendChild(imageElement);
-        worksElement.appendChild(titleElement);
-        sectionGallery.appendChild(worksElement)
-             });
-
-    });
-
- // Boutons filtres terminés //
-
- //Partie bouton modifier et modale // 
+// Modale // 
+const modalContainer = document.querySelector("#modal-container")
+const openModal = document.querySelector("#open-modal")
+const modalClose = document.querySelector("#close-modal")
+openModal.addEventListener("click", function(event){
+    (modalContainer),
+    modalContainer.style.display = "block";
+})
+modalClose.addEventListener("click", function(){
+    (modalContainer)
+    modalContainer.style.display = "none";
+})
 
 
